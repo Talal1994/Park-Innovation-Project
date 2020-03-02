4a77840a479ed4d42826ee5d1e5bff3b08d45f8c
Mhanna-Jonblat <61241856+Mhanna-Jonblat@users.noreply.github.com>
2020-03-02 19:46:50 +0200

    admin headeer

--- Main/View/Admin/adminHeader.php
+++ Main/View/Admin/adminHeader.php
@@ -13,11 +13,11 @@ Released   : 20121012
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-    <meta name="keywords" content="" />
-    <meta name="description" content="" />
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <title>University Management System</title>
-        
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
@@ -39,8 +39,7 @@ Released   : 20121012
         <!-- end #header -->
         <div id="menu">
             <ul>
-                <li
-                ><a href="studentClasses.php">Classes</a></li>
+                <li><a href="studentClasses.php">Classes</a></li>
                 <li><a href="studentRegistration.php">Registration</a></li>
                 <li><a href="studentProfile.php">Profile</a></li>
                 <li class="last"><a href="../Login.php">Log Out</a></li>
